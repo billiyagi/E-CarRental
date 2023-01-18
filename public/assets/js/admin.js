e0ec52dda81b778b7a71d011fa325acf88b4ed05
Febry Billiyagi <89958256+billiyagi@users.noreply.github.com>
2023-01-18 20:39:38 +0700

    Update: add authentication based on simple API
    Menambahkan sebuah authentikasi dengan mengambil data dari API sederhana lalu dikonsumsi dengan JavaScript.

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -1,7 +1,20 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
 	// Cek Status Authentikasi
-	if ( localStorage.getItem('status_login') != 1 ) {
-		$('#failLoginModal').modal('show')
+	if (
+		(!localStorage.getItem("username") && !localStorage.getItem("name")) ||
+		(localStorage.getItem("username") == "" &&
+			localStorage.getItem("name") == "")
+	) {
+		$("#failLoginModal").modal("show");
+	} else {
+		$("#userFullName").text(localStorage.getItem("name"));
 	}
-})
+
+	$("#logoutBtn").click(function () {
+		if (confirm("Yakin ingin keluar?")) {
+			localStorage.removeItem("username");
+			localStorage.removeItem("name");
+			window.location.href = "/index.html";
+		}
+	});
+});
